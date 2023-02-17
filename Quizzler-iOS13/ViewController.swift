5bf80f7ac32dae42e7809c6292a35b25da9e141c
Michael Jones <jones.mej@gmail.com>
2023-02-17 14:03:53 -0500

    Endless quiz

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var questionLabel: UILabel!
+  @IBOutlet weak var progressBar: UIProgressView!
+  @IBOutlet weak var falseButton: UIButton!
+  @IBOutlet weak var trueButton: UIButton!
+  
+  var questionNumber = 0
+  
+  let quiz = [
+    ["Four + Two is equal to Six", "True"],
+    ["Five - Three is greater than One", "True"],
+    ["Three + Eight is less than Ten", "False"]
+  ]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    updateUI()
+  }
+  
+  @IBAction func answerButtonPressed(_ sender: UIButton) {
+    let userAnswer = sender.currentTitle
+    let actualAnswer = quiz[questionNumber][1]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    if actualAnswer == userAnswer {
+      print("Right")
+    } else {
+      print("Wrong")
+    }
+    
+    if (questionNumber < quiz.count - 1) {
+      questionNumber += 1
+    } else {
+      questionNumber = 0
     }
 
+    updateUI()
 
+  }
+
+  func updateUI() {
+    questionLabel.text = quiz[questionNumber][0]
+  }
 }
 
